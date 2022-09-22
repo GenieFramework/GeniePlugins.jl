515686b2a9772e9bc1d976df3076d60fcc088849
Abhi <8083613+AbhimanyuAryan@users.noreply.github.com>
2022-09-23 00:58:23 +0530

    Fixing typo Genie.Plugins

--- src/GeniePlugins.jl
+++ src/GeniePlugins.jl
@@ -65,13 +65,13 @@ function congrats()
   Markdown.doc"""
   ```julia
   function install(dest::String; force = false) :: Nothing
-    src = abspath(normpath(joinpath(@__DIR__, "..", Genie.Plugins.FILES_FOLDER)))
+    src = abspath(normpath(joinpath(@__DIR__, "..", GeniePlugins.FILES_FOLDER)))
 
     for f in readdir(src)
       isfile(f) && continue
       isdir(f) || mkpath(joinpath(src, f))
 
-      Genie.Plugins.install(joinpath(src, f), dest, force = force)
+      GeniePlugins.install(joinpath(src, f), dest, force = force)
     end
 
     nothing
@@ -103,13 +103,14 @@ function scaffold(plugin_name::String, dest::String = "."; force = false)
     module $plugin_name
 
     using Genie
+    using GeniePlugins
 
     function install(dest::String; force = false)
-      src = abspath(normpath(joinpath(@__DIR__, "..", Genie.Plugins.FILES_FOLDER)))
+      src = abspath(normpath(joinpath(@__DIR__, "..", GeniePlugins.FILES_FOLDER)))
 
       for f in readdir(src)
         isdir(f) || continue
-        Genie.Plugins.install(joinpath(src, f), dest, force = force)
+        GeniePlugins.install(joinpath(src, f), dest, force = force)
       end
     end
 
